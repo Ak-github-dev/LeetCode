66ede1078052658739860837b3ff535ad7c8d9ac
Aamir Kolhar <104218169+Ak-github-dev@users.noreply.github.com>
2024-09-02 11:28:09 +0530

    Time: 0 ms (100%), Space: 44.9 MB (5.43%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -0,0 +1,41 @@
+/*
+Approach:
+
+Two Pointers:
+
+Use two pointers: one to track the odd nodes and one to track the even nodes.
+Traverse the list, connecting odd-indexed nodes together and even-indexed nodes together.
+
+Reconnection:
+
+After the traversal, connect the last odd-indexed node to the first even-indexed node to form the final list.
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+        return head;
+    }
+}
